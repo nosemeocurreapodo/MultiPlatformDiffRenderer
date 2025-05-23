da3229834b6aef0e63b33285bfb5b20c9b299df0
nosemeocurreapodo <emanueltrabes@gmail.com>
2025-05-23 20:12:35 +0200

    fix: small update

--- src/backends/xrt/depthrendererfpga.cpp
+++ src/backends/xrt/depthrendererfpga.cpp
@@ -4,26 +4,28 @@
 #include "meshfpga.h"
 #include "rendererfpga.h"
 
-void DepthRenderFPGA(float *pos_buffer_data,
-                     float *tex_buffer_data,
-                     float *wei_buffer_data,
-                     unsigned int *ebo_buffer_data,
-                     fpga::ImageType *in_texture_data,
-                     fpga::Scalar *out_texture_data,
-                     unsigned int pos_buffer_size,
-                     unsigned int tex_buffer_size,
-                     unsigned int wei_buffer_size,
-                     unsigned int ebo_buffer_size,
-                     unsigned int in_texture_width,
-                     unsigned int in_texture_height,
-                     unsigned int in_texture_channels,
-                     unsigned int out_texture_width,
-                     unsigned int out_texture_height,
-                     unsigned int out_texture_channels,
-                     float q_x, float q_y, float q_z, float q_w,
-                     float t_x, float t_y, float t_z,
-                     float fx, float fy, float cx, float cy)
+extern "C"
 {
+    void DepthRenderFPGA(float *pos_buffer_data,
+                         float *tex_buffer_data,
+                         float *wei_buffer_data,
+                         unsigned int *ebo_buffer_data,
+                         fpga::ImageType *in_texture_data,
+                         fpga::Scalar *out_texture_data,
+                         unsigned int pos_buffer_size,
+                         unsigned int tex_buffer_size,
+                         unsigned int wei_buffer_size,
+                         unsigned int ebo_buffer_size,
+                         unsigned int in_texture_width,
+                         unsigned int in_texture_height,
+                         unsigned int in_texture_channels,
+                         unsigned int out_texture_width,
+                         unsigned int out_texture_height,
+                         unsigned int out_texture_channels,
+                         float q_x, float q_y, float q_z, float q_w,
+                         float t_x, float t_y, float t_z,
+                         float fx, float fy, float cx, float cy)
+    {
 #pragma HLS INTERFACE m_axi port = pos_buffer_data bundle = gmem0
 #pragma HLS INTERFACE m_axi port = tex_buffer_data bundle = gmem0
 #pragma HLS INTERFACE m_axi port = wei_buffer_data bundle = gmem0
@@ -31,16 +33,17 @@ void DepthRenderFPGA(float *pos_buffer_data,
 #pragma HLS INTERFACE m_axi port = in_texture_data bundle = gmem0
 #pragma HLS INTERFACE m_axi port = out_texture_data bundle = gmem0
 
-    BufferFPGA<float> pos_buffer(pos_buffer_size, pos_buffer_data);
-    BufferFPGA<float> tex_buffer(tex_buffer_size, tex_buffer_data);
-    BufferFPGA<float> wei_buffer(wei_buffer_size, wei_buffer_data);
-    BufferFPGA<unsigned int> ebo_buffer(ebo_buffer_size, ebo_buffer_data);
-    MeshFPGA mesh(pos_buffer, tex_buffer, wei_buffer, ebo_buffer);
-    TextureFPGA<fpga::ImageType> in_texture(in_texture_width, in_texture_height, in_texture_channels, 0, in_texture_data);
-    TextureFPGA<float> out_texture(out_texture_width, out_texture_height, out_texture_channels, 0, out_texture_data);
-    fpga::SE3 pose(fpga::SO3(fpga::Quaternion(q_w, q_x, q_y, q_z)), fpga::Vec3(t_x, t_y, t_z));
-    fpga::Camera cam(fx, fy, cx, cy);
+        BufferFPGA<float> pos_buffer(pos_buffer_size, pos_buffer_data);
+        BufferFPGA<float> tex_buffer(tex_buffer_size, tex_buffer_data);
+        BufferFPGA<float> wei_buffer(wei_buffer_size, wei_buffer_data);
+        BufferFPGA<unsigned int> ebo_buffer(ebo_buffer_size, ebo_buffer_data);
+        MeshFPGA mesh(pos_buffer, tex_buffer, wei_buffer, ebo_buffer);
+        TextureFPGA<fpga::ImageType> in_texture(in_texture_width, in_texture_height, in_texture_channels, 0, in_texture_data);
+        TextureFPGA<float> out_texture(out_texture_width, out_texture_height, out_texture_channels, 0, out_texture_data);
+        fpga::SE3 pose(fpga::SO3(fpga::Quaternion(q_w, q_x, q_y, q_z)), fpga::Vec3(t_x, t_y, t_z));
+        fpga::Camera cam(fx, fy, cx, cy);
 
-    DepthRendererFPGA renderer;
-    renderer.Render(mesh, pose, cam, in_texture, out_texture, 0);
+        DepthRendererFPGA renderer;
+        renderer.Render(mesh, pose, cam, in_texture, out_texture, 0);
+    }
 }
