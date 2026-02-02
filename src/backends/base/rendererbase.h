646386293366532a9a7c005a7afb173c96e3df28
NOSEMEOCURREAPODO <emanueltrabes@gmail.com>
2026-02-02 10:28:07 +0100

    fix: small changes

--- src/backends/base/rendererbase.h
+++ src/backends/base/rendererbase.h
@@ -1051,8 +1051,8 @@ public:
         IntType y_m = y - 1;
         IntType y_mm = y - 2;
 
-        if (x_p >= width || x_m < 0 || y_p >= height || y_m < 0) // ||
-                                                                 // x_pp >= width || x_mm < 0 || y_pp >= height || y_mm < 0)
+        if (x_p >= width || x_m < 0 || y_p >= height || y_m < 0 ||
+            x_pp >= width || x_mm < 0 || y_pp >= height || y_mm < 0)
         {
             //  No need to explicitly set to nodata, it is already in the background color
             return;
@@ -1063,10 +1063,10 @@ public:
         ImageType f_y_m = intextures.in_texture(y_m, x);
         ImageType f_x_p = intextures.in_texture(y, x_p);
         ImageType f_x_m = intextures.in_texture(y, x_m);
-        // ImageType f_y_pp = intextures.in_texture.texel_(y_pp, x, uniforms.in_lvl);
-        // ImageType f_y_mm = intextures.in_texture.texel_(y_mm, x, uniforms.in_lvl);
-        // mageType f_x_pp = intextures.in_texture.texel_(y, x_pp, uniforms.in_lvl);
-        // ImageType f_x_mm = intextures.in_texture.texel_(y, x_mm, uniforms.in_lvl);
+        ImageType f_y_pp = intextures.in_texture(y_pp, x);
+        ImageType f_y_mm = intextures.in_texture(y_mm, x);
+        ImageType f_x_pp = intextures.in_texture(y, x_pp);
+        ImageType f_x_mm = intextures.in_texture(y, x_mm);
 
         // if (f_x_p == nodata || f_x_m == nodata ||
         //     f_y_p == nodata || f_y_m == nodata || f == nodata)
@@ -1076,11 +1076,11 @@ public:
         //}
 
         Vec3<RealType> out_fragment;
-        // out_fragment(0) = (-RealType(f_x_pp) + RealType(8) * RealType(f_x_p) - RealType(8) * RealType(f_x_m) + RealType(f_x_mm)) / RealType(12);
-        // out_fragment(1) = (-RealType(f_y_pp) + RealType(8) * RealType(f_y_p) - RealType(8) * RealType(f_y_m) + RealType(f_y_mm)) / RealType(12);
+        out_fragment(0) = (-RealType(f_x_pp) + RealType(8) * RealType(f_x_p) - RealType(8) * RealType(f_x_m) + RealType(f_x_mm)) / RealType(12);
+        out_fragment(1) = (-RealType(f_y_pp) + RealType(8) * RealType(f_y_p) - RealType(8) * RealType(f_y_m) + RealType(f_y_mm)) / RealType(12);
         // out_fragment(2) = RealType(f);
-        out_fragment(0) = (RealType(f_x_p) - RealType(f_x_m)) / RealType(2);
-        out_fragment(1) = (RealType(f_y_p) - RealType(f_y_m)) / RealType(2);
+        // out_fragment(0) = (RealType(f_x_p) - RealType(f_x_m)) / RealType(2);
+        // out_fragment(1) = (RealType(f_y_p) - RealType(f_y_m)) / RealType(2);
         out_fragment(2) = RealType(0); // f; // save the projected frame for later processing
 
         // if(out_fragment.norm() < RealType(50))
@@ -1516,8 +1516,8 @@ public:
 
         RealType kf = sample<RealType, TextureViewRead<ImageType>>(intextures.kf_texture,
                                                                    texcoord(1), texcoord(0));
-        // Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture.texel_(gl_FragCoord(1), gl_FragCoord(0), uniforms.out_lvl);
-        Vec3<RealType> d_f_d_xy = sample<Vec3<RealType>, TextureViewRead<Vec3<float>>>(intextures.dfdxy_texture, texcoord(1), texcoord(0));
+        Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture(gl_FragCoord(1), gl_FragCoord(0));
+        // Vec3<RealType> d_f_d_xy = sample<Vec3<RealType>, TextureViewRead<Vec3<float>>>(intextures.dfdxy_texture, texcoord(1), texcoord(0));
 
         // if (kf == intextures.kf_texture.nodata() || f == intextures.f_texture.nodata() || d_f_d_xy == intextures.dfdxy_texture.nodata())
         //     return;
@@ -1525,7 +1525,8 @@ public:
         RealType f_exp = apply_exposure(RealType(kf), uniforms.exposure);
         RealType d_fexp_d_f = d_f_exp_d_f(RealType(kf), uniforms.exposure);
         Vec3<RealType> d_fexp_d_exp = d_f_exp_d_exp(RealType(kf), uniforms.exposure);
-        Vec3<RealType> d_fexp_d_xy = d_fexp_d_f * d_f_d_xy;
+        // Vec3<RealType> d_fexp_d_xy = d_fexp_d_f * d_f_d_xy;
+        Vec3<RealType> d_fexp_d_xy = d_f_d_xy;
 
         RealType v0 = d_fexp_d_xy(0) * uniforms.fx * RealType(out_width) / f_ver(2);
         RealType v1 = d_fexp_d_xy(1) * uniforms.fy * RealType(out_height) / f_ver(2);
@@ -2085,7 +2086,7 @@ public:
             return;
 
         RealType kf = sample<RealType, TextureViewRead<ImageType>>(intextures.kf_texture, texcoord(1), texcoord(0));
-        // Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture.texel_(gl_FragCoord(1), gl_FragCoord(0), uniforms.out_lvl);
+        // Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture(gl_FragCoord(1), gl_FragCoord(0));
         Vec3<RealType> d_f_d_xy = sample<Vec3<RealType>, TextureViewRead<Vec3<float>>>(intextures.dfdxy_texture, texcoord(1), texcoord(0));
 
         // if (kf == intextures.kf_texture.nodata() || f == intextures.f_texture.nodata() || d_f_d_xy == intextures.dfdxy_texture.nodata())
@@ -2094,7 +2095,8 @@ public:
         RealType f_exp = apply_exposure(RealType(kf), uniforms.exposure);
         RealType d_fexp_df = d_f_exp_d_f(RealType(kf), uniforms.exposure);
         Vec3<RealType> d_fexp_d_exp = d_f_exp_d_exp(RealType(kf), uniforms.exposure);
-        Vec3<RealType> d_fexp_d_xy = d_fexp_df * d_f_d_xy;
+        // Vec3<RealType> d_fexp_d_xy = d_fexp_df * d_f_d_xy;
+        Vec3<RealType> d_fexp_d_xy = d_f_d_xy;
 
         Vec3<RealType> d_f_i_d_f_ver;
 
@@ -2864,7 +2866,7 @@ public:
 
         RealType kf = sample<RealType, TextureViewRead<ImageType>>(intextures.kf_texture,
                                                                    texcoord(1), texcoord(0));
-        // Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture.texel_(gl_FragCoord(1), gl_FragCoord(0), uniforms.out_lvl);
+        // Vec3<RealType> d_f_d_xy = intextures.dfdxy_texture(gl_FragCoord(1), gl_FragCoord(0));
         Vec3<RealType> d_f_d_xy = sample<Vec3<RealType>, TextureViewRead<Vec3<float>>>(intextures.dfdxy_texture, texcoord(1), texcoord(0));
 
         // if (kf == intextures.kf_texture.nodata() || f == intextures.f_texture.nodata() || d_f_d_xy == intextures.dfdxy_texture.nodata())
@@ -2874,6 +2876,7 @@ public:
         RealType d_fexp_df = d_f_exp_d_f(RealType(kf), uniforms.exposure);
         Vec3<RealType> d_fexp_d_exp = d_f_exp_d_exp(RealType(kf), uniforms.exposure);
         Vec3<RealType> d_fexp_d_xy = d_fexp_df * d_f_d_xy;
+        // Vec3<RealType> d_fexp_d_xy = d_f_d_xy;
 
         Vec3<RealType> d_f_i_d_f_ver;
 
